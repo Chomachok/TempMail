4e20d552ddf1b6f075faab04a028f8fa859f1e56
Evgeniy <evgeniydmit13@gmail.com>
2025-11-01 15:26:51 +0400

    Вывод почты в нужном поле при её создании. Добавлены попытки на EmailReciver (не работает, нужно доделать)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,25 +3,65 @@ using TempMail.Services;
 
 namespace TempMail.Controllers;
 
-public class HomeController(ILogger<HomeController> logger) : Controller
+public class HomeController : Controller
 {
+    private const string SessionEmailKey = "UserEmail";
+    private const string SessionEmailCreatedKey = "EmailCreated";
+    private readonly TimeSpan _emailLifeTime = TimeSpan.FromMinutes(10);
     private readonly EmailGenerator _emailGenerator = new();
+    private string? _email;
 
     public IActionResult Index()
     {
+        var emailCreated = HttpContext.Session.GetString(SessionEmailCreatedKey) == "true";
+
+        if (emailCreated)
+        {
+            ViewData["Email"] = HttpContext.Session.GetString(SessionEmailKey);
+            ViewData["ShowTimer"] = true;
+        }
+        else
+        {
+            ViewData["Email"] = "";
+            ViewData["ShowTimer"] = false;
+        }
+
         return View();
     }
+
+    [NonAction]
+    public string? GetEmail() => _email;
     
     [HttpPost]
     public IActionResult GenerateEmail()
     {
-        var email = _emailGenerator.GenerateEmail();
-        logger.LogInformation("Created email {Email}", email);
-        var emailGenerated = true;
-        ViewData["Email"] = email;
-        ViewData["EmailGenerated"] = emailGenerated;
+        _email = _emailGenerator.GenerateEmail();
+        // logger.LogInformation("Created email {Email}", _email);
         
-        return View("Index");
+        HttpContext.Session.SetString(SessionEmailKey, _email);
+        HttpContext.Session.SetString(SessionEmailCreatedKey, "true");
+        
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult ResetEmail()
+    {
+        HttpContext.Session.Remove(SessionEmailKey);
+        HttpContext.Session.Remove(SessionEmailCreatedKey);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult RefreshEmail()
+    {
+        var email = _emailGenerator.GenerateEmail();
+        
+        HttpContext.Session.SetString(SessionEmailKey, email);
+        HttpContext.Session.SetString(SessionEmailCreatedKey, "true");
+
+        return RedirectToAction("Index");
     }
     
     public IActionResult Contacts() => View();
--- obj/Debug/net8.0/TempMail.AssemblyInfo.cs
+++ obj/Debug/net8.0/TempMail.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TempMail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdf986c92171962e84776519173bf70a862a622")]
 [assembly: System.Reflection.AssemblyProductAttribute("TempMail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TempMail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
